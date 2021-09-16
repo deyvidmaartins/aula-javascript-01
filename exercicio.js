a1866205bba79e2446506bbfd8ad129ae4c10723
Deyvid Martins <deyvid.silva123@gmail.com>
2021-09-16 18:13:39 -0300

    add file

---
+++ exercicio.js
@@ -0,0 +1,31 @@
+// sua idade var
+
+var numero = 18;
+console.log("Número");
+console.log(numero);
+
+//dia de hoje var
+
+var dia_de_hoje = 16;
+console.log("Dia de hoje");
+console.log(dia_de_hoje);
+
+//nome let
+
+let nome = "joão";
+console.log(nome);
+
+//idade let
+
+let idade = 13;
+console.log(idade);
+  
+//nome const 
+ 
+const nome = "mário";
+console.log(nome);
+
+//pai const
+
+const pai = "josé";
+console.log(pai);
